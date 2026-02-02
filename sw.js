e029dd4977234788d4afcb915d8cae9da6a679ab
Serg Dort <sergdort@gmail.com>
2026-02-02 22:11:43 +0000

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "cb6fd4824d5bac9418aa5e6e55e6c16c"
+    "revision": "443ba6bba68d57769b1010b5cffb5052"
   },
   {
     "url": "styles.953cc786368e91057aee.css"
